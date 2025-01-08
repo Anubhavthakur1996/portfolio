af43cd0bcb76b64845334d9f6ad6bf2583024ac8
Thakur Anubhav Singh <anubhavthakur1996@outlook.com>
2025-01-09 00:36:49 +0530

    design changes on contact me page

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,69 +6,71 @@ const Contact = () => {
   return (
     <div className="contact">
       <span className="title">You can find me on:</span>
-      <div className="contact-list shadow-one">
-        {/* LinkedIn */}
-        <div className="contact-row">
-          <span className="contact-icon">
-            <FaLinkedin style={{ color: "#0a66c2" }} />
-          </span>
-          <span className="contact-info">
-            <a
-              href="https://www.linkedin.com/in/anubhavthakur1996/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              LinkedIn
-            </a>
-          </span>
-        </div>
+      <div className="contact-list-container">
+        <div className="contact-list shadow-one">
+          {/* LinkedIn */}
+          <div className="contact-row">
+            <span className="contact-icon">
+              <FaLinkedin style={{ color: "#0a66c2" }} />
+            </span>
+            <span className="contact-info">
+              <a
+                href="https://www.linkedin.com/in/anubhavthakur1996/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                LinkedIn
+              </a>
+            </span>
+          </div>
 
-        {/* Git Hub */}
-        <div className="contact-row">
-          <span className="contact-icon">
-            <FaGitAlt style={{ color: "#f54739" }} />
-          </span>
-          <span className="contact-info">
-            <a
-              href="https://www.linkedin.com/in/anubhavthakur1996/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              GitHub
-            </a>
-          </span>
-        </div>
+          {/* Git Hub */}
+          <div className="contact-row">
+            <span className="contact-icon">
+              <FaGitAlt style={{ color: "#f54739" }} />
+            </span>
+            <span className="contact-info">
+              <a
+                href="https://www.linkedin.com/in/anubhavthakur1996/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                GitHub
+              </a>
+            </span>
+          </div>
 
-        {/* E-Mail */}
-        <div className="contact-row">
-          <span className="contact-icon">
-            <MdEmail style={{ color: "#0078d4" }} />
-          </span>
-          <span className="contact-info">
-            <a
-              href="https://www.linkedin.com/in/anubhavthakur1996/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              E-Mail
-            </a>
-          </span>
-        </div>
+          {/* E-Mail */}
+          <div className="contact-row">
+            <span className="contact-icon">
+              <MdEmail style={{ color: "#0078d4" }} />
+            </span>
+            <span className="contact-info">
+              <a
+                href="https://www.linkedin.com/in/anubhavthakur1996/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                E-Mail
+              </a>
+            </span>
+          </div>
 
-        {/* Number */}
-        <div className="contact-row">
-          <span className="contact-icon">
-            <FcCellPhone />
-          </span>
-          <span className="contact-info">
-            <a
-              href="https://www.linkedin.com/in/anubhavthakur1996/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              ***********
-            </a>
-          </span>
+          {/* Number */}
+          <div className="contact-row">
+            <span className="contact-icon">
+              <FcCellPhone />
+            </span>
+            <span className="contact-info">
+              <a
+                href="https://www.linkedin.com/in/anubhavthakur1996/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                ***********
+              </a>
+            </span>
+          </div>
         </div>
       </div>
     </div>
